ff2f4e863ab3180bae1f1d93dcb2eb3bd3f8b99e
nasser <cherniinasriii@gmail.com>
2023-06-02 18:13:29 +0100

    Updated code to include new features

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -24,7 +24,7 @@ const Contacts = () => {
    </div>
    <h1 className='font-NFonts text-base text-white mb-52 mr-4'>Email</h1>
    <h1 className='font-NFonts text-base text-white mt-36  absolute'>cherniinasriii@gmail.com</h1>
-  <h1 className="text-base mt-60 font-NFonts  absolute text-white tracking-wide cursor-pointer hover:scale-125    hover:text-designColor duration-1000  border-designColor">send a message</h1>
+  <a href='https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&osid=1&passive=1209600&service=mail&ifkv=Af_xneGUJwhzcxtpfOHr0VJjMHjYXSh7Ax3Cai6SYEDJra4WvLEWQKTcNN29CKO62TAsPXzsSoBb&flowName=GlifWebSignIn&flowEntry=ServiceLogin' className="text-base mt-60 font-NFonts  absolute text-white tracking-wide cursor-pointer hover:scale-125    hover:text-designColor duration-1000  border-designColor">send a message</a>
 
 
    </div>
@@ -34,7 +34,8 @@ const Contacts = () => {
    </div>
    <h1 className='font-NFonts text-base text-white mb-52 mr-4'>Facebook</h1>
    <h1 className='font-NFonts text-base text-white mt-36 -ml-5  absolute'>Na sser</h1>
-  <h1 className="text-base mt-60 font-NFonts  absolute text-white tracking-wide cursor-pointer hover:scale-125    hover:text-designColor duration-1000  border-designColor">send a message</h1>
+   
+  <a  href='https://www.facebook.com/nasri.cherni.75/' className="text-base mt-60 font-NFonts  absolute text-white tracking-wide cursor-pointer hover:scale-125    hover:text-designColor duration-1000  border-designColor">send a message</a>
 
 
    </div>
@@ -44,7 +45,7 @@ const Contacts = () => {
    </div>
    <h1 className='font-NFonts text-base text-white mb-52 mr-4'>Whatsapp</h1>
    <h1 className='font-NFonts text-base text-white mt-36  l absolute'>50349860</h1>
-  <h1 className="text-base mt-60 font-NFonts  absolute text-white tracking-wide cursor-pointer hover:scale-125    hover:text-designColor duration-1000  border-designColor">send a message</h1>
+  <a href='https://web.whatsapp.com/' className="text-base mt-60 font-NFonts  absolute text-white tracking-wide cursor-pointer hover:scale-125    hover:text-designColor duration-1000  border-designColor">send a message</a>
 
 
    </div>
