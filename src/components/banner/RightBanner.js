7a72ec4c6472b03c08a163cf2782dd7022634fe9
cherninasri <cherniinasriii@gmail.com>
2023-06-17 16:47:19 +0100

    Update description 2

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,11 +5,12 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
-        className="w-[300px] h-[400px] lgl:w-[650px] lgl:h-[580px] z-10 "
-        src={bannerImg}
+        className=" lgl:w-[670px] lgl:h-[480px] z-10 "
+        src={bannerImg2}
         alt="bannerImg"
+
       />
-      <div className="absolute bottom-12   w-[350px] h-[100px] lgl:w-[480px] lgl:h-[420px] bg-designColor  rounded-bor  "></div>
+      <div className="absolute bottom-0 w-[350px] h-[100px] lgl:w-[480px] lgl:h-[420px] bg-designColor  rounded-bor  "></div>
     </div>
   );
 }
